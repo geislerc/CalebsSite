4051cab1090abd2b010cb00069af1e5754438b62
geislerc <geislerc@uwm.edu>
2023-12-27 17:44:49 -0600

    first commit

--- CalebsGithub/views.py
+++
@@ -1,9 +0,0 @@
-from django.views import View
-from django.shortcuts import render, redirect, get_object_or_404
-
-
-class HomePage(View):
-    def get(self, request):
-        template = 'index.html'
-
-        return render(request, template)
